c20aed469e7d216804114c5f266fd533919ab90a
786 <sh.n.n786@gmail.com>
2025-05-23 21:50:22 +0330

    fix bug

--- RadioOnline/app/Providers/AppServiceProvider.php
+++ RadioOnline/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\Interfaces\MediaServiceInterface;
 use App\Services\MediaService;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
