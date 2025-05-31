34a72ce9f7b0ada31fc0ee32c53f9cf74db47f5e
786 <sh.n.n786@gmail.com>
2025-05-31 20:04:23 +0330

    ``` fix: update channel name in welcome view
    Updated the constant `channel_name` from "stream" to "stream1" in the `welcome.blade.php` view to ensure the correct audio stream is loaded.
    ```

--- RadioOnline/app/Http/Middleware/VisitorsMiddleware.php
+++ RadioOnline/app/Http/Middleware/VisitorsMiddleware.php
@@ -14,6 +14,8 @@ class VisitorsMiddleware
      *
      * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
+
+
     public function handle(Request $request, Closure $next): Response
     {
         Visitor::create([
--- RadioOnline/bootstrap/app.php
+++ RadioOnline/bootstrap/app.php
@@ -18,7 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(VisitorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
