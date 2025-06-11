bd99946326fd44a415ca1f83265f3823d6449943
786 <sh.n.n786@gmail.com>
2025-06-11 22:52:03 +0330

    Commit message for the change using the conventional commit convention:
    ```
    feat(config): add stream_url configuration for Icecast
    
    Added a new configuration option 'stream_url' to the Icecast configuration file. This allows specifying the stream URL, with a default value set to 'http://icecast:1990'. This enhancement improves the flexibility of stream URL management in different environments.
    ```

--- RadioOnline/config/icecast.php
+++ RadioOnline/config/icecast.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'stream_url' => env('ICECAST_STREAM_URL', 'http://icecast:1990'),
     'host' => env('ICECAST_HOST', 'http://icecast:1990'),
     'username' => env('ICECAST_USERNAME', 'admin'),
     'password' => env('ICECAST_PASSWORD', 'hackme'),
